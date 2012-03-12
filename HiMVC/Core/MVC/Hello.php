3f13c2e8b7cdf393bb6c77ce772a76367ea511aa
André R <ar@ez.no>
2012-03-13 00:56:59 +0100

    Added Hello->doWorld action for testing use
    Also make this serve the request by default.

---
+++ HiMVC/Core/MVC/Hello.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Content Controller
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 2009-2012 github.com/andrerom. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License v3
+ * @version //autogentag//
+ */
+
+namespace HiMVC\Core\MVC;
+use HiMVC\Core\MVC\Request;
+
+/**
+ * Example controller, does no chnages to data atm
+ */
+class Hello
+{
+    /**
+     * @var \HiMVC\Core\MVC\Request
+     */
+    protected $request;
+
+    /**
+     * @param \HiMVC\Core\MVC\Request $request
+     */
+    public function __construct( Request $request )
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Add new item in collection ( ie GET /hello/ )
+     *
+     * @return \HiMVC\Core\MVC\Response
+     */
+    public function doWorld()
+    {
+        return __METHOD__ . "()";
+    }
+}
+
