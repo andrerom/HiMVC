7897cc03c6bea7b8bb48a0407e3ea347b193d880
André R <ar@ez.no>
2012-03-22 21:35:17 +0100

    Rename and cleraify comments in index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 /**
  * index.php
  *
- * This file on purpose does not use any PHP 5 language features to be able to exit with
- * message about wrong php version even on PHP 4.
+ * This file on purpose does not use any PHP 5(.3) language features to be able to exit with
+ * message about wrong php version even on PHP 4 or 5.2.
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @copyright Copyright (C) 2009-2012 github.com/andrerom. All rights reserved.
@@ -15,13 +15,13 @@
 // Make sure we are on php 5.3 and validate request_order if not in cli mode
 if ( version_compare( PHP_VERSION, '5.3' ) < 0 )
 {
-    echo '<h1>eZ Publish does not like your PHP version: ' . PHP_VERSION . '</h1>';
+    echo '<h1>HiMVC does not like your PHP version: ' . PHP_VERSION . '</h1>';
     echo '<p>PHP 5.3.0 or higher is required!</p>';
     exit;
 }
 else if ( PHP_SAPI !== 'cli' && ini_get( 'request_order' ) !== 'GP' )
 {
-    echo '<h1>eZ Publish does not like your <a href="http://no2.php.net/manual/en/ini.core.php#ini.request-order">request_order</a> value: ' . ini_get('request_order'). '</h1>';
+    echo '<h1>HiMVC does not like your <a href="http://no2.php.net/manual/en/ini.core.php#ini.request-order">request_order</a> value: ' . ini_get('request_order'). '</h1>';
     echo '<p>Only \'GP\' is supported due to security concerns!</p>';
     exit;
 }
